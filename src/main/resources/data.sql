f2e6f479c1ac2cbecfb52e9fb2b89cf2e431935d
Mark-Kil <markkilcanarias@gmail.com>
2024-01-19 08:55:53 +0100

    more info added to sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,6 +3,7 @@ INSERT INTO types (id_type, type) VALUES (default, 'Capybara');
 INSERT INTO types (id_type, type) VALUES (default, 'Scorpion');
 INSERT INTO types (id_type, type) VALUES (default, 'Rabbit');
 INSERT INTO types (id_type, type) VALUES (default, 'Red Panda');
+INSERT INTO types (id_type, type) VALUES (default, 'León');
 
 /* Families */
 INSERT INTO families (id_family, family) VALUES (default, 'Félidos');
@@ -16,7 +17,11 @@ INSERT INTO genders (id_gender, gender) VALUES (default, 'Male');
 INSERT INTO genders (id_gender, gender) VALUES (default, 'Female');
 
 /* Animals */
-INSERT INTO animals (id_animal, name, admission_date, type_id, family_id, gender_id) VALUES (default, 'Mark', CURRENT_TIMESTAMP, 1, 1, 1);
+INSERT INTO animals (id_animal, name, admission_date, type_id, family_id, gender_id) VALUES (default, 'Vero', CURRENT_TIMESTAMP, 1, 4, 2);
+INSERT INTO animals (id_animal, name, admission_date, type_id, family_id, gender_id) VALUES (default, 'Dani', CURRENT_TIMESTAMP, 2, 3, 1);
+INSERT INTO animals (id_animal, name, admission_date, type_id, family_id, gender_id) VALUES (default, 'Brus', CURRENT_TIMESTAMP, 3, 5, 2);
+INSERT INTO animals (id_animal, name, admission_date, type_id, family_id, gender_id) VALUES (default, 'Romina', CURRENT_TIMESTAMP, 4, 2, 2);
+INSERT INTO animals (id_animal, name, admission_date, type_id, family_id, gender_id) VALUES (default, 'Mark', CURRENT_TIMESTAMP, 5, 1, 1);
 
 /* Roles */
 INSERT INTO roles (id_role, name) VALUES (default, 'ROLE_ADMIN');
